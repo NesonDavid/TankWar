12db8700ca9aa69b75e3125f8836945f5cf0bbed
David <1153510430@qq.com>
2016-08-06 10:50:32 +0800

    TankWar1.0

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,26 +1,87 @@
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by user on 2016/7/31.
- */
-public class TankClient extends Frame{
-    public static void main(String[] args) {
-        new TankClient().launchFrame();
+public class TankClient extends Frame {
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
+
+    Tank myTank = new Tank(50,50,this);
+    List<Missile> missiles = new ArrayList<>();
+
+    Image offScreenImage = null;
+
+    public void paint(Graphics g) {
+        g.drawString("missilesCount:" + missiles.size(),10,50);
+
+        myTank.draw(g);
+        for(int i=0; i<missiles.size(); i++) {
+            Missile m = missiles.get(i);
+            m.draw(g);
+        }
+    }
+
+    public void update(Graphics g) {
+        if(offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.GREEN);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     public void launchFrame() {
-        setLocation(300,50);
-        setSize(800,600);
-        setTitle("TankWar");
-        addWindowListener(new WindowAdapter() {
-            @Override
+        this.setLocation(300, 100);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
+        this.setTitle("TankWar");
+        this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-        setResizable(false);
+        this.setResizable(false);
+        this.setBackground(Color.GREEN);
+
+        this.addKeyListener(new KeyMonitor());
+
         setVisible(true);
+
+        new Thread(new PaintThread()).start();
+    }
+
+    public static void main(String[] args) {
+        TankClient tc = new TankClient();
+        tc.launchFrame();
+    }
+
+    private class PaintThread implements Runnable {
+
+        public void run() {
+            while(true) {
+                repaint();
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private class KeyMonitor extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            myTank.keyPressed(e);
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            myTank.keyReleased(e);
+        }
     }
 }
